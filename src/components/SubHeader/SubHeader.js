912e954a906374cf2c4e678d17dfef06cf56aff0
arthurmichaelw <arthurmichaelw@gmail.com>
2023-01-25 22:23:20 -0500

    basic html created for HomeCard, SubCard, and SubHeader

---
+++ src/components/SubHeader/SubHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import './SubHeader.scss'
+
+function SubHeader() {
+  return (
+    <div className='SubHeaderContainer'>
+        <h1 className='SubHeaderTitle'>`${props.title}`</h1>
+        <h4 className='SubHeaderPath'>${}</h4>
+        {/* <button /> if joined then display leave else display join */}
+    </div>
+  )
+}
+
+export default SubHeader
