6675aed05013f9df79ea4a40e7293464c08599d4
arthurmichaelw <arthurmichaelw@gmail.com>
2023-02-03 16:15:42 -0500

    jasons subheader

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,13 +1,15 @@
 import React from 'react'
-// import './SubHeader.scss'
+import './SubHeader.scss'
 
 export default function SubHeader({currentSub}) {
   return (
     <div className='SubHeaderContainer'>
-        <h1 className='SubHeaderTitle'>Community Title: {currentSub.subName}</h1>
-        <h3>Community Creator: {currentSub.subOwner.username}</h3>
-        <h4 className='SubHeaderPath'>Path: {`/s/${currentSub._id}`}</h4>
+      <div className='SubHeaderSizer'>
+        <h1 className='SubHeaderTitle'> {currentSub.subName}</h1>
+        <h3 className='SubHeaderUsername'> s/{currentSub.subOwner.username}</h3>
+        {/* <h4 className='SubHeaderPath'>Path: {`/s/${currentSub._id}`}</h4> */}
         {/* <button /> if joined then display leave else display join */}
+      </div>
     </div>
   )
 }
